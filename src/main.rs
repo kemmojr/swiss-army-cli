095c630891c2c08a2d9043c65ba95c740a63a4dd
Tim Kemmis <timothydkemmis@gmail.com>
2025-02-11 13:08:09 +1100

    🔎 beginning the implementation of ls

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,55 @@
+use std::process::exit;
+use std::{env, usize};
+
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
 fn main() {
     println!("Welcome to my swiss army CLI utility.");
     println!("Use --help for a list of all currently implemented commands.");
 
-    let supported_commands: [&str; 1] = ["ls"];
-    for command in supported_commands {
-        let output: &str = match command {
-            "ls" => "unfortunately I have just gotten started on this command",
-            _ => "",
+    let args: Vec<String> = env::args().collect();
+
+    let args_len = args.len();
+    match args_len {
+        x if x > 1 => println!("The command that will be executed is '{}'", args[1]),
+        x if x > 0 => {
+            println!("Please pass in the command you wish to run after the 'cargo run' command");
+            return;
+        }
+
+        _ => {
+            println!("No args provided");
+            return;
+        }
+    }
+
+    let output: &str = match &args[1] {
+            c if c == "ls" => "unfortunately I have just gotten started on this command and it isn't fully functional yet",
+            _ => "This command is not yet supported. To view a list of commands, 'cargo run' with --help",
         };
 
-        println!("{}", output)
-    }
+    println!("{}", output)
+
+    /*
+       // Create a path to the desired file
+           let path = Path::new("hello.txt");
+           let display = path.display();
+
+           // Open the path in read-only mode, returns `io::Result<File>`
+           let mut file = match File::open(&path) {
+               Err(why) => panic!("couldn't open {}: {}", display, why),
+               Ok(file) => file,
+           };
+
+           // Read the file contents into a string, returns `io::Result<usize>`
+           let mut s = String::new();
+           match file.read_to_string(&mut s) {
+               Err(why) => panic!("couldn't read {}: {}", display, why),
+               Ok(_) => print!("{} contains:\n{}", display, s),
+           }
+
+           // `file` goes out of scope, and the "hello.txt" file gets closed
+    */
 }
